5b8d336d283f98cbf70f8ea5f8221196144ca563
Daniel Johnson <danielrjohnson00@gmail.com>
2023-06-12 10:38:27 -0600

    integrate EnvProv into env plugins

--- dsi/plugins/env.py
+++ dsi/plugins/env.py
@@ -6,6 +6,7 @@ A home for environment plugin parsers.
 from collections import OrderedDict
 import os
 import socket
+import subprocess
 
 from dsi.plugins.driver import PluginDriver
 
@@ -54,3 +55,58 @@ class HostnamePlugin(EnvPluginDriver):
             self.output_collector[key] = []
         self.output_collector['hostname'] = []
         self.add_row()
+
+
+
+class EnvProvPlugin(EnvPluginDriver):
+    """
+    Plugin for reading environment provenance data:
+    1. System Kernel Version
+    2. Kernel compile-time config
+    3. Kernel boot config
+    4. Kernel runtime config
+    5. Kernel modules and module config
+    6. Container information, if containerized
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+
+
+    def parse(self) -> None:
+        k_ver = self.get_kernel_version()
+        #k_ct_conf = self.get_kernel_ct_config()
+        #k_bt_conf = self.get_kernel_bt_config()
+        #k_rt_conf = self.get_kernel_rt_config()
+        #TODO: modules
+        #TODO: containers
+        self.output_collector["kernel version"] = k_ver
+
+    def add_row(self) -> None:
+        #TODO: figure out what goes here
+        pass
+
+    def get_kernel_version(self) -> str:
+        return self.get_cmd_output(["uname", "-r"])
+
+
+    def get_kernel_ct_config(self):
+        pass
+
+
+    def get_kernel_bt_config(self):
+        pass
+
+
+    def get_kernel_rt_config(self):
+        pass
+
+
+    @staticmethod
+    def get_cmd_output(cmd: list) -> str:
+        proc = subprocess.run(cmd, capture_output=True)
+        if proc.stderr != b"":
+            raise Exception(proc.stderr)
+        return proc.stdout.strip().decode("utf-8")
+
+
