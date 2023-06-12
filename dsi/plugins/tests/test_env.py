5a5d3256c19f34299378786cf3060f764b24c35c
Daniel Johnson <danielrjohnson00@gmail.com>
2023-06-12 10:38:41 -0600

    add initial EnvProv tests

--- dsi/plugins/tests/test_env.py
+++ dsi/plugins/tests/test_env.py
@@ -1,6 +1,6 @@
 import collections
 
-from dsi.plugins.env import HostnamePlugin
+from dsi.plugins.env import HostnamePlugin, EnvProvPlugin
 
 def test_hostnameplugin_type():
     a = HostnamePlugin()
@@ -15,3 +15,14 @@ def test_hostname_plugin_col_shape():
     a.add_row()
     a.add_row()
     assert len(a.output_collector.keys())==len(a.output_collector.values())
+
+def test_envprov_plugin_type():
+    plug = EnvProvPlugin()
+    plug.parse()
+    assert type(plug.output_collector) == collections.OrderedDict
+
+def test_envprov_plugin_kver_type():
+    plug = EnvProvPlugin()
+    plug.parse()
+    k_ver = plug.output_collector["kernel version"]
+    assert type(k_ver) == str
