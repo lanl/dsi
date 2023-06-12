ecbf38971fccfcce05eab1f6d6c74b15182031fa
Quincy Wofford <quincy.wofford@gmail.com>
2023-06-12 09:53:09 -0600

    Improve software packaging, incorporate sphinx documentation creation workflow (#10)
    * try github actions
    
    * improve rst formatting for landing readme
    
    * improve demo section readme formatting
    
    * add Makefile to clean up documentation generation procedure
    
    * checkpoint for troubleshooting
    
    * move to setuptools build strategy
    
    * add readme for building documentation
    
    * improve documentation and exclude documentation readme from RTD
    
    * add section on managing virtual environments
    
    * Update README.rst
    
    Still learning RST formatting syntax
    
    * Still learning rst syntax
    
    * Update README.rst
    
    * Update README.rst
    
    * Update README.rst
    
    Caught a typo
    
    * fix incorrect installation documentation
    
    * changing named dsi section to project section to improve packaging syntax for python setuptools
    
    * boost recommended python version
    
    * correcting some metadata syntax in pyproject.toml
    
    ---------
    
    Co-authored-by: qwofford <qwofford@lanl.gov>
    Co-authored-by: Daniel Johnson <danieljohnson@pn1614910.lanl.gov>

--- docs/conf.py
+++ docs/conf.py
@@ -14,10 +14,10 @@ release = '0.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx_rtd_theme']
+extensions = ['sphinx_rtd_theme','sphinx.ext.autodoc']
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'README.rst']
 
 
 
