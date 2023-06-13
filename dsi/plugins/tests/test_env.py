855e8662fb1ecd8e67ffcb8007ff121ca68dce4e
Daniel Johnson <danielrjohnson00@gmail.com>
2023-06-13 10:13:39 -0600

    add more envprov tests

--- dsi/plugins/tests/test_env.py
+++ dsi/plugins/tests/test_env.py
@@ -18,11 +18,31 @@ def test_hostname_plugin_col_shape():
 
 def test_envprov_plugin_type():
     plug = EnvProvPlugin()
-    plug.parse()
+    plug.add_row()
+
+    #for key in plug.output_collector:
+    #    print(key, ":", plug.output_collector[key])
+    #print(len(plug.output_collector))
+
     assert type(plug.output_collector) == collections.OrderedDict
 
 def test_envprov_plugin_kver_type():
     plug = EnvProvPlugin()
-    plug.parse()
-    k_ver = plug.output_collector["kernel version"]
+    plug.add_row()
+
+    k_ver = plug.output_collector["kernel version"][0]
     assert type(k_ver) == str
+
+def test_envprov_plugin_adds_rows():
+    plug = EnvProvPlugin()
+    plug.add_row()
+    plug.add_row()
+
+    for key, val in plug.output_collector.items():
+        assert len(val) == 2
+
+def test_envprov_plugin_has_many_columns():
+    plug = EnvProvPlugin()
+    plug.add_row()
+
+    assert len(plug.output_collector.keys()) > 100 # should def have more than 100
