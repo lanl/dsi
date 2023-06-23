6c301289918472cf3850f6ff9d37116dfabbad59
Quincy Wofford <qwofford@lanl.gov>
2023-06-23 10:14:44 -0600

    checkpoint for travel

--- dsi/core.py
+++ dsi/core.py
@@ -22,15 +22,18 @@ class Terminal():
         static_munge = \
         lambda prefix,implementations: ['.'.join(i) for i in product(prefix,implementations)]
 
-        self.drivers = []
+        self.driver_collection = []
         driver_modules = static_munge(self.DRIVER_PREFIX, self.DRIVER_IMPLEMENTATIONS)
         for module in driver_modules:
-            self.drivers.append(import_module(module))
+            self.driver_collection.append(import_module(module))
 
-        self.plugins = []
+        self.plugin_collections = []
         plugin_modules = static_munge(self.PLUGIN_PREFIX, self.PLUGIN_IMPLEMENTATIONS)
         for module in plugin_modules:
-            self.plugins.append(import_module(module))
+            self.plugin_collections.append(import_module(module))
+
+        self.active_plugins = []
+        self.active_drivers = []
 
     def list_available_modules(self,mod_type):
         """
@@ -43,17 +46,25 @@ class Terminal():
         # Helper function to determine if module is in a collection
         mod_is_present = lambda modname, mod_collection: \
         sum([modname in dir(mod) for mod in mod_collection])
+
         valid_mods=[]
         if mod_type=='plugin':
             for valid_plugin in self.VALID_PLUGINS:
-                if mod_is_present(valid_plugin, self.plugins):
+                if mod_is_present(valid_plugin, self.plugin_collections):
                     valid_mods.append(valid_plugin)
         if mod_type=='driver':
             for valid_driver in self.VALID_DRIVERS:
-                if mod_is_present(valid_driver, self.drivers):
+                if mod_is_present(valid_driver, self.driver_collection):
                     valid_mods.append(valid_driver)
         print('Loadable {}s: {}'.format(mod_type,', '.join(valid_mods)))
-          
+
+        def load_module(mod_type, mod_name):
+            if mod_type=='plugin' and mod_name in self.plugin_collection:
+                print('Plugin {} already loaded. Nothing to do.'.format(mod_name))
+            if mod_type
+            for plugin in self.plugin_collection:
+                try:
+                 
 # Core capability requirements:
 # 	Init plugin, parse data
 # 	Store backend
