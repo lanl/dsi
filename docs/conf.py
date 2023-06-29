31422324810af4acf25a5cad3bbfde1737b01bfb
Quincy Wofford <quincy.wofford@gmail.com>
2023-06-29 16:01:40 -0600

    fix conf.py style formatting

--- docs/conf.py
+++ docs/conf.py
@@ -14,15 +14,14 @@ release = '0.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx_rtd_theme','sphinx.ext.autodoc']
+extensions = ['sphinx_rtd_theme', 'sphinx.ext.autodoc']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'README.rst']
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-#html_static_path = ['_static']
+# html_static_path = ['_static']
