0ea156079978275c67672878a3658e389d096b7e
Quincy Wofford <quincy.wofford@gmail.com>
2023-06-29 15:38:33 -0600

    style changes for test_env.py

--- dsi/plugins/env.py
+++ dsi/plugins/env.py
@@ -1,16 +1,18 @@
-from collections import OrderedDict 
+from collections import OrderedDict
 import os
 import socket
 import subprocess
 from getpass import getuser
 from git import Repo
+import git.exc
 
 from dsi.plugins.metadata import StructuredMetadata
+
 
 class Environment(StructuredMetadata):
     """
     Environment Plugins inspect the calling process' context.
-    
+
     Environments assume a POSIX-compliant filesystem and always collect UID/GID
     information.
     """
@@ -25,6 +27,7 @@ class Environment(StructuredMetadata):
         self.posix_info['moniker'] = getuser()
         moniker = self.posix_info['moniker']
         self.posix_info['gid_list'] = os.getgrouplist(moniker, egid)
+
 
 class Hostname(Environment):
     """
@@ -50,6 +53,7 @@ class Hostname(Environment):
         row = list(self.posix_info.values()) + [socket.gethostname()]
         self.add_to_output(row)
 
+
 class Bueno(Environment):
     """
     A Plugin to capture performance data from Bueno (github.com/lanl/bueno)
@@ -65,26 +69,28 @@ class Bueno(Environment):
 
     def pack_header(self) -> None:
         """Set schema with POSIX and Bueno data."""
-        column_names = list(self.posix_info.keys()) + list(self.bueno_data.keys())
+        column_names = list(self.posix_info.keys()) + \
+            list(self.bueno_data.keys())
         self.set_schema(column_names)
 
     def add_row(self) -> None:
         """Parses environment provenance data and adds the row."""
         if not self.schema_is_set():
-            with open(self.filename,'r') as fh:
-                file_content=(fh.read())
+            with open(self.filename, 'r') as fh:
+                file_content = (fh.read())
             rows = file_content.split('\n')
             drop_rows = [row for row in rows if row != '']
             rows = drop_rows
             for row in rows:
                 colon_split = row.split(':')
-                if len(colon_split)!=2:
+                if len(colon_split) != 2:
                     raise TypeError
                 self.bueno_data[colon_split[0]] = colon_split[1]
             self.pack_header()
 
         row = list(self.posix_info.values()) + list(self.bueno_data.values())
         self.add_to_output(row)
+
 
 class GitInfo(Environment):
     """
@@ -98,9 +104,10 @@ class GitInfo(Environment):
         super().__init__()
         try:
             self.repo = Repo(git_repo_path)
-        except:
+        except git.exc:
             raise Exception(f"Git could not find .git/ in {git_repo_path}, " +
-                             "GitInfo Plugin must be given a repo base path (default is working dir)")
+                            "GitInfo Plugin must be given a repo base path " +
+                            "(default is working dir)")
         self.git_info = {
             "git-remote": lambda: self.repo.git.remote("-v"),
             "git-commit": lambda: self.repo.git.rev_parse("--short", "HEAD")
@@ -108,7 +115,8 @@ class GitInfo(Environment):
 
     def pack_header(self) -> None:
         """ Set schema with POSIX and Git columns """
-        column_names = list(self.posix_info.keys()) + list(self.git_info.keys())
+        column_names = list(self.posix_info.keys()) + \
+            list(self.git_info.keys())
         self.set_schema(column_names)
 
     def add_row(self) -> None:
@@ -116,8 +124,10 @@ class GitInfo(Environment):
         if not self.schema_is_set():
             self.pack_header()
 
-        row = list(self.posix_info.values()) + [self.git_info["git-remote"](), self.git_info["git-commit"]()]
+        row = list(self.posix_info.values()) + \
+            [self.git_info["git-remote"](), self.git_info["git-commit"]()]
         self.add_to_output(row)
+
 
 class SystemKernel(Environment):
     """
@@ -137,12 +147,11 @@ class SystemKernel(Environment):
         super().__init__()
         self.prov_info = self.get_prov_info()
 
-
     def pack_header(self) -> None:
         """Set schema with keys of prov_info."""
-        column_names = list(self.posix_info.keys()) + list(self.prov_info.keys()) 
+        column_names = list(self.posix_info.keys()) + \
+            list(self.prov_info.keys())
         self.set_schema(column_names)
-
 
     def add_row(self) -> None:
         """Parses environment provenance data and adds the row."""
@@ -150,8 +159,8 @@ class SystemKernel(Environment):
             self.pack_header()
 
         pairs = self.get_prov_info()
-        self.add_to_output(list(self.posix_info.values()) + list(pairs.values()))
-        
+        self.add_to_output(list(self.posix_info.values()) +
+                           list(pairs.values()))
 
     def get_prov_info(self) -> dict:
         """Collect and return the different categories of provenance info."""
@@ -164,15 +173,14 @@ class SystemKernel(Environment):
         prov_info.update(self.get_container_config())
         return prov_info
 
-
     def get_kernel_version(self) -> dict:
         """Kernel version is obtained by the "uname -r" command, returns it in a dict. """
         return {"kernel version": self.get_cmd_output(["uname -r"])}
 
-
     def get_kernel_ct_config(self) -> dict:
         """
-        Kernel compile-time configuration is collected by looking at /boot/config-(kernel version) and removing comments and empty lines.
+        Kernel compile-time configuration is collected by looking at /boot/config-(kernel version)
+        and removing comments and empty lines.
 
         The output of said command is newline-delimited option=value pairs.
         """
@@ -180,29 +188,28 @@ class SystemKernel(Environment):
         res = self.get_cmd_output(command)
         lines = res.split("\n")
         ct_config = {}
-        for line in lines: # each line is an option=value pair
+        for line in lines:  # each line is an option=value pair
             option, value = line.split("=", maxsplit=1)
             ct_config[option] = value
         return ct_config
-
 
     def get_kernel_bt_config(self) -> dict:
         """
         Kernel boot-time configuration is collected by looking at /proc/cmdline.
 
-        The output of this command is one string of 
-        boot-time parameters. This string is returned in a dict.
+        The output of this command is one string of boot-time parameters. This string is
+        returned in a dict.
         """
         command = ["cat /proc/cmdline"]
         res = self.get_cmd_output(command)
         return {"cat /proc/cmdline": res}
 
-
     def get_kernel_rt_config(self) -> dict:
         """
         Kernel run-time configuration is collected with the "sysctl -a" command.
 
-        The output of this command is lines consisting of two possibilities:            option = value (note the spaces), and sysctl: permission denied ...
+        The output of this command is lines consisting of two possibilities:
+        option = value (note the spaces), and sysctl: permission denied ...
         The option = value pairs are added to the output dict.
         """
         command = ["sysctl -a 2>&1"]
@@ -210,12 +217,12 @@ class SystemKernel(Environment):
         lines = res.split("\n")
         rt_config = {}
         for line in lines:
-            if "=" in line: # if the line is not permission denied
-                option, value = line.split(" = ", maxsplit=1) # note the spaces
+            if "=" in line:  # if the line is not permission denied
+                option, value = line.split(
+                    " = ", maxsplit=1)  # note the spaces
                 rt_config[option] = value
             # If line is permission denied, ignore it.
         return rt_config
-
 
     def get_kernel_mod_config(self) -> dict:
         """
@@ -227,26 +234,26 @@ class SystemKernel(Environment):
         modules = self.get_cmd_output(lsmod_command).split("\n")
 
         sep = "END MODINFO"
-        modinfo_command = ["/sbin/modinfo $(lsmod | tail -n +2 | awk '{print $1}' | sed 's/nvidia_/nvidia-current-/g' | sed 's/^nvidia$/nvidia-current/g') | "
-                            f"sed -e 's/filename:/{sep}filename:/g'"]
-        modinfos = self.get_cmd_output(modinfo_command, ignore_stderr=True).split("\n" + sep)
+        modinfo_command = ["/sbin/modinfo $(lsmod | tail -n +2 | awk '{print $1}' | \
+                           sed 's/nvidia_/nvidia-current-/g' | \
+                           sed 's/^nvidia$/nvidia-current/g') | "
+                           f"sed -e 's/filename:/{sep}filename:/g'"]
+        modinfos = self.get_cmd_output(
+            modinfo_command, ignore_stderr=True).split("\n" + sep)
 
         mod_configs = {}
         for mod, info in zip(modules, modinfos):
             mod_configs["/sbin/modinfo " + mod] = info
         return mod_configs
 
-
     def get_container_config(self) -> dict:
         # Not yet implemented
         return {}
-
 
     def update_output(self, pairs: dict) -> None:
         """Appends a given dict's values to the output under the same key."""
         for key, val in pairs.items():
             self.output[key].append(val)
-
 
     @staticmethod
     def get_cmd_output(cmd: list, ignore_stderr=False) -> str:
@@ -259,4 +266,3 @@ class SystemKernel(Environment):
         if proc.stderr != b"" and not ignore_stderr:
             raise Exception(proc.stderr)
         return proc.stdout.strip().decode("utf-8")
-
