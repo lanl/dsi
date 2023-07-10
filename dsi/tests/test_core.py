f0df807a903d14e050d45a3a4543fa8c7b7c2367
Daniel Johnson <danieljohnson@lanl.gov>
2023-07-10 15:22:54 -0600

    Add module unloading + test

--- dsi/tests/test_core.py
+++ dsi/tests/test_core.py
@@ -6,3 +6,12 @@ def test_terminal_module_getter():
     plugins = a.list_available_modules('plugin')
     drivers = a.list_available_modules('driver')
     assert len(plugins) > 0 and len(drivers) > 0
+
+
+def test_unload_module():
+    a = Terminal()
+    print(a.list_loaded_modules())
+    a.load_module('plugin', 'GitInfo', 'producer')
+    assert len(a.list_loaded_modules()['producer']) == 1
+    a.unload_module('plugin', 'GitInfo', 'producer')
+    assert len(a.list_loaded_modules()['producer']) == 0
