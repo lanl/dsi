aa809c330727a758646549e8baad50b190d4f9f8
Daniel Johnson <danieljohnson@lanl.gov>
2023-07-18 11:57:37 -0600

    use get_git_root instead of cwd

--- dsi/tests/test_core.py
+++ dsi/tests/test_core.py
@@ -1,4 +1,11 @@
+import git
 from dsi.core import Terminal
+
+
+def get_git_root(path):
+    git_repo = git.Repo(path, search_parent_directories=True)
+    git_root = git_repo.git.rev_parse("--show-toplevel")
+    return (git_root)
 
 
 def test_terminal_module_getter():
@@ -10,7 +17,8 @@ def test_terminal_module_getter():
 
 def test_unload_module():
     a = Terminal()
-    a.load_module('plugin', 'GitInfo', 'producer')
+    a.load_module('plugin', 'GitInfo', 'producer',
+                  git_repo_path=get_git_root('.'))
     assert len(a.list_loaded_modules()['producer']) == 1
     a.unload_module('plugin', 'GitInfo', 'producer')
     assert len(a.list_loaded_modules()['producer']) == 0
