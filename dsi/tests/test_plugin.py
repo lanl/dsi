8fc44d26dcf6993b4170d0c94747ba9c700d62b0
Vedant P Iyer <viyer@pn2405703.lanl.gov>
2024-09-05 22:03:52 -0600

    Fixed updated test assert statements

--- dsi/tests/test_plugin.py
+++ dsi/tests/test_plugin.py
@@ -4,13 +4,16 @@ import subprocess
 import os
 
 def test_export_db_erd():
-    subprocess.run(["sqlite3", "erd_test.db"], stdin= open("examples/data/erd_test.sql", "r"))
+    subprocess.run(["sqlite3", "erd_test.db"], stdin= open("../../examples/data/erd_test.sql", "r"))
     fw.ER_Diagram("erd_test.db").export_erd("erd_test.db", "erd_test_output")
     os.remove("erd_test.db")
+    output = cv2.imread("erd_test_output.png")
+    assert output is not None #check if image generated at all
 
-    assert cv2.imread("erd_test_output.png") is not None #check if image generated at all
-    
-    assert open("examples/data/er-diagram.png","rb").read() == open("erd_test_output.png","rb").read() #check if er diagram matches reference image
+    reference = cv2.imread("../../examples/data/er-diagram.png")
+
+    assert output.shape == reference.shape
+    assert cv2.countNonZero(cv2.cvtColor(cv2.absdiff(output, reference), cv2.COLOR_BGR2GRAY)) == 0
     os.remove("erd_test_output.png")
 
 test_export_db_erd()
