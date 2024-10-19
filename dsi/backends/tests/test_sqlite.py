d0ee9f90446dfd4f765e5371dde628d66ca62c42
Vedant P Iyer <viyer@pn2405703.lanl.gov>
2024-10-18 22:32:57 -0600

    Updated last test to handle nested ordered dicts

--- dsi/backends/tests/test_sqlite.py
+++ dsi/backends/tests/test_sqlite.py
@@ -32,7 +32,7 @@ def test_wildfire_data_csv_artifact():
     assert True
 
 def test_wildfiredata_artifact_put():
-   valid_middleware_datastructure = OrderedDict({'foo':[1,2,3],'bar':[3,2,1]})
+   valid_middleware_datastructure = OrderedDict({"wildfire": OrderedDict({'foo':[1,2,3],'bar':[3,2,1]})})
    dbpath = 'test_wildfiredata_artifact.sqlite_data'
    store = Sqlite(dbpath)
    store.put_artifacts(valid_middleware_datastructure)
