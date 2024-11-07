717676018de2a09d455826425f3b1d8b3e2081a1
Vedant P Iyer <viyer@pn2405703.lanl.gov>
2024-11-07 01:01:13 -0700

    Moved erd test function to test_file_writer and deleted test_plugin.py/CI file

--- dsi/plugins/tests/test_file_writer.py
+++ dsi/plugins/tests/test_file_writer.py
@@ -2,8 +2,11 @@ from dsi.core import Terminal
 from collections import OrderedDict
 import git
 
-import dsi.plugins.file_writer as wCSV
+# import dsi.plugins.file_writer as wCSV
 from dsi.backends.sqlite import Sqlite
+import cv2
+import numpy as np
+import os
 
 def get_git_root(path):
     git_repo = git.Repo(path, search_parent_directories=True)
@@ -15,3 +18,18 @@ def test_csv_plugin_type():
     back = Sqlite(filename=path)
     
     #assert type(plug.output_collector) == OrderedDict
+
+def test_export_db_erd():
+    a=Terminal(debug_flag=False)
+    a.load_module('plugin', 'Schema', 'reader', filename="examples/data/example_schema.json" , target_table_prefix = "student")
+    a.load_module('plugin', 'YAML', 'reader', filenames=["examples/data/student_test1.yml", "examples/data/student_test2.yml"], target_table_prefix = "student")
+    a.load_module('plugin', 'TOML', 'reader', filenames=["examples/data/results.toml"], target_table_prefix = "results")
+    a.load_module('plugin', 'ER_Diagram', 'writer', filename = 'erd_test_output.png')
+    a.transload()
+    
+    er_image = cv2.imread("erd_test_output.png")
+    assert er_image is not None #check if image generated at all
+    
+    pixel_mean = np.mean(er_image)
+    os.remove("erd_test_output.png")
+    assert pixel_mean != 255 #check if image is all white pixels (no diagram generated)
