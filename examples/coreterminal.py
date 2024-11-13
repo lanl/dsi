5a68c19802b5bec4bc79e3a74d079fcaf6db5e65
Vedant P Iyer <viyer@pn2405703.lanl.gov>
2024-11-13 16:14:33 -0700

    added inspect artifact handler function example workflow

--- examples/coreterminal.py
+++ examples/coreterminal.py
@@ -3,24 +3,25 @@ from dsi.core import Terminal
 
 '''This is an example workflow using core.py'''
 
-a=Terminal(debug_flag=False)
+a=Terminal(debug_flag=False, backup_db_flag=False)
 
 # a.load_module('plugin','Bueno','reader', filenames='data/bueno1.data')
 # a.load_module('plugin','Hostname','reader')
 
-# a.load_module('plugin', 'Schema', 'reader', filename="data/example_schema.json" , target_table_prefix = "student")
-# a.load_module('plugin', 'YAML', 'reader', filenames=["data/student_test1.yml", "data/student_test2.yml"], target_table_prefix = "student")
-# a.load_module('plugin', 'TOML', 'reader', filenames=["data/results.toml"], target_table_prefix = "results")
+a.load_module('plugin', 'Schema', 'reader', filename="data/example_schema.json" , target_table_prefix = "student")
+a.load_module('plugin', 'YAML', 'reader', filenames=["data/student_test1.yml", "data/student_test2.yml"], target_table_prefix = "student")
+a.load_module('plugin', 'TOML', 'reader', filenames=["data/results.toml"], target_table_prefix = "results")
 
 # a.load_module('plugin', "Table_Plot", "writer", table_name = "schema__physics", filename = "schema__physics")
 # a.load_module('plugin', 'ER_Diagram', 'writer', filename = 'er_diagram.pdf')#, target_table_prefix = "physics")
-# a.transload()
+a.transload()
 
-# a.load_module('backend','Sqlite','back-write', filename='data/data.db')
+a.load_module('backend','Sqlite','back-write', filename='data/data.db')
 # a.load_module('backend','Parquet','back-write',filename='data/bueno.pq')
 
-# a.artifact_handler(interaction_type='put')
+a.artifact_handler(interaction_type='put')
 # data = a.artifact_handler(interaction_type='get', query = "SELECT * FROM run_table;")#, isVerbose = True)
+a.artifact_handler(interaction_type="inspect")
 # print(data)
 
 # a.unload_module('backend', 'Sqlite', 'back-write')
@@ -49,8 +50,8 @@ a=Terminal(debug_flag=False)
 
 
 #Example use 2
-a.load_module('backend','SqliteReader','back-read', filename='data/data.db')   
-a.artifact_handler(interaction_type="read")
-a.load_module('plugin', 'ER_Diagram', 'writer', filename = 'er_diagram.pdf')#, target_table_prefix = "physics")
-a.load_module('plugin', "Table_Plot", "writer", table_name = "student__physics", filename = "student__physics")
-a.transload()
+# a.load_module('backend','SqliteReader','back-read', filename='data/data.db')   
+# a.artifact_handler(interaction_type="read")
+# a.load_module('plugin', 'ER_Diagram', 'writer', filename = 'er_diagram.pdf')#, target_table_prefix = "physics")
+# a.load_module('plugin', "Table_Plot", "writer", table_name = "student__physics", filename = "student__physics")
+# a.transload()
