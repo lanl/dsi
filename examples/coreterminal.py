c9e0845850d23230794a0a85cd3c190874d413e3
Vedant P Iyer <viyer@pn2405703.lanl.gov>
2024-11-15 15:17:21 -0700

    Updated with new toml and yaml reader names

--- examples/coreterminal.py
+++ examples/coreterminal.py
@@ -8,20 +8,20 @@ a=Terminal(debug_flag=False, backup_db_flag=False)
 # a.load_module('plugin','Bueno','reader', filenames='data/bueno1.data')
 # a.load_module('plugin','Hostname','reader')
 
-# a.load_module('plugin', 'Schema', 'reader', filename="data/example_schema.json" , target_table_prefix = "student")
-# a.load_module('plugin', 'YAML', 'reader', filenames=["data/student_test1.yml", "data/student_test2.yml"])
-# a.load_module('plugin', 'TOML', 'reader', filenames=["data/results.toml"], target_table_prefix = "results")
+# a.load_module('plugin', 'Schema', 'reader', filename="data/example_schema.json", target_table_prefix = "student")
+# a.load_module('plugin', 'YAML1', 'reader', filenames=["data/student_test1.yml", "data/student_test2.yml"], target_table_prefix = "student")
+# a.load_module('plugin', 'TOML1', 'reader', filenames=["data/results.toml"], target_table_prefix = "results")
 
-# a.load_module('plugin', "Table_Plot", "writer", table_name = "schema__physics", filename = "schema__physics")
-# a.load_module('plugin', 'ER_Diagram', 'writer', filename = 'er_diagram.pdf')#, target_table_prefix = "physics")
-a.transload()
+# # a.load_module('plugin', "Table_Plot", "writer", table_name = "schema__physics", filename = "schema__physics")
+# # a.load_module('plugin', 'ER_Diagram', 'writer', filename = 'er_diagram.pdf')#, target_table_prefix = "physics")
+# a.transload()
 
-a.load_module('backend','Sqlite','back-write', filename='data/data.db')
-# a.load_module('backend','Parquet','back-write',filename='data/bueno.pq')
+# a.load_module('backend','Sqlite','back-write', filename='data/data.db')
+# # a.load_module('backend','Parquet','back-write',filename='data/bueno.pq')
 
-a.artifact_handler(interaction_type='put')
-# data = a.artifact_handler(interaction_type='get', query = "SELECT * FROM run_table;")#, isVerbose = True)
-a.artifact_handler(interaction_type="inspect")
+# a.artifact_handler(interaction_type='put')
+# # data = a.artifact_handler(interaction_type='get', query = "SELECT * FROM run_table;")#, isVerbose = True)
+# a.artifact_handler(interaction_type="read")
 # print(data)
 
 # a.unload_module('backend', 'Sqlite', 'back-write')
@@ -50,7 +50,7 @@ a.artifact_handler(interaction_type="inspect")
 
 
 #Example use 2
-# a.load_module('backend','Sqlite','back-read', filename='data/data.db')   
-# a.artifact_handler(interaction_type="read")
-# a.load_module('plugin', 'ER_Diagram', 'writer', filename = 'er_diagram.png')#, target_table_prefix = "physics")
-# a.transload()
+a.load_module('backend','Sqlite','back-read', filename='data/data.db')   
+a.artifact_handler(interaction_type="read")
+a.load_module('plugin', 'ER_Diagram', 'writer', filename = 'er_diagram.png')#, target_table_prefix = "physics")
+a.transload()
