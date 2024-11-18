e59c7da720a8ae67d773872bd8314bcd72d994c1
Vedant P Iyer <viyer@pn2405703.lanl.gov>
2024-11-18 00:53:55 -0700

    updated pennant example to use dsi object and new get_artifact dict return to update abstraction

--- examples/pennant/create_and_query_dsi_db.py
+++ examples/pennant/create_and_query_dsi_db.py
@@ -8,6 +8,9 @@ Then it creates a DSI db from the csv file and performs a query.
 import argparse
 import sys
 from dsi.backends.sqlite import Sqlite, DataType
+import os
+
+from dsi.core import Terminal
 
 isVerbose = True
 
@@ -15,29 +18,29 @@ isVerbose = True
 Creates the DSI db from the csv file
 """
 
-def import_pennant_data(test_name):
-    csvpath = 'pennant_' + test_name + '.csv'
-    dbpath = 'pennant_' + test_name + '.db'
-    store = Sqlite(dbpath)
-    store.put_artifacts_csv(csvpath, "rundata", isVerbose=isVerbose)
-    store.close()
-    # No error implies success
+# def import_pennant_data(test_name):
+#     csvpath = 'pennant_' + test_name + '.csv'
+#     dbpath = 'pennant_' + test_name + '.db'
+#     store = Sqlite(dbpath)
+#     store.put_artifacts_csv(csvpath, "rundata", isVerbose=isVerbose)
+#     store.close()
+#     # No error implies success
 
 """
 Performs a sample query on the DSI db
 """
-def test_artifact_query(test_name):
-    dbpath = "pennant_" + test_name + ".db"
-    store = Sqlite(dbpath)
-    _ = store.get_artifact_list(isVerbose=isVerbose)
-    data_type = DataType()
-    data_type.name = "rundata"
-    query = "SELECT * FROM " + str(data_type.name) + \
-      " where hydro_cycle_run_time > 0.006"
-    print("Running Query", query)
-    result = store.sqlquery(query)
-    store.export_csv(result, "pennant_query.csv")
-    store.close()
+# def test_artifact_query(test_name):
+#     dbpath = "pennant_" + test_name + ".db"
+#     store = Sqlite(dbpath)
+#     _ = store.get_artifacts(isVerbose=isVerbose)
+#     data_type = DataType()
+#     data_type.name = "rundata"
+#     query = "SELECT * FROM " + str(data_type.name) + \
+#       " where hydro_cycle_run_time > 0.006"
+#     print("Running Query", query)
+#     result = store.sqlquery(query)
+#     store.export_csv(result, "pennant_query.csv")
+#     store.close()
 
 if __name__ == "__main__":
     """ The testname argument is required """
@@ -49,6 +52,36 @@ if __name__ == "__main__":
         parser.print_help()
         sys.exit(0)
     
-    import_pennant_data(test_name)
-    test_artifact_query(test_name)
+    table_name = "rundata"
+    csvpath = 'pennant_' + test_name + '.csv'
+    dbpath = 'pennant_' + test_name + '.db'
+    output_csv = "pennant_read_query.csv"
 
+    #read in csv
+    core = Terminal()
+    core.load_module('plugin', "Csv", "reader", filenames = csvpath, table_name = table_name)
+    core.transload()
+
+    if os.path.exists(dbpath):
+        os.remove(dbpath)
+    #load to sqlite db
+    core.load_module('backend','Sqlite','back-write', filename=dbpath, run_table = False)
+    core.artifact_handler(interaction_type='put')
+
+    #OLD -- read in csv and load to sqlite db
+    # import_pennant_data(test_name)
+
+    # update dsi abstraction using query to sqlite db
+    query_data = core.artifact_handler(interaction_type='get', query = f"SELECT * FROM {table_name} WHERE hydro_cycle_run_time > 0.006;", dict_return = True)
+    core.update_abstraction(table_name, query_data)
+
+    # ALTERNATE WAY TO UPDATE DB
+    # core.load_module('backend','Sqlite','back-read', filename=dbpath, run_table = False)
+    # core.artifact_handler(interaction_type='read', query=f"SELECT * FROM {table_name} WHERE hydro_cycle_run_time > 0.006;")
+
+    #export to csv
+    core.load_module('plugin', "Csv_Writer", "writer", filename = output_csv, table_name = table_name)
+    core.transload()
+
+    #OLD -- get part of db and export that to a csv
+    # test_artifact_query(test_name)
