2950f7f28679be4a8c62ca691a9623cd4aa65b9c
Vedant1 <vedantiyer@hotmail.com>
2025-01-21 18:28:17 -0700

    new find function to search backend, deleted pass through code, new names for put/get functions to overwrite originals later

--- dsi/backends/filesystem.py
+++ dsi/backends/filesystem.py
@@ -16,11 +16,27 @@ class Backend(metaclass=ABCMeta):
         pass
 
     @abstractmethod
-    def get_artifacts(self, query):
+    def get_artifacts(self, query, kwargs):
         pass
 
     @abstractmethod
-    def inspect_artifacts(self):
+    def write_artifacts(self, artifacts, kwargs) -> None:
+        pass
+
+    @abstractmethod
+    def process_artifacts(self, query, kwargs):
+        pass
+
+    @abstractmethod
+    def inspect_artifacts(self, kwargs):
+        pass
+    
+    @abstractmethod
+    def read_to_artifacts(self, kwargs):
+        pass
+
+    @abstractmethod
+    def find(self, query_object, kwargs):
         pass
 
 
@@ -48,8 +64,20 @@ class Filesystem(Backend):
     def put_artifacts(self, artifacts, kwargs) -> None:
         pass
 
-    def get_artifacts(self, query):
+    def get_artifacts(self, query, kwargs):
         pass
 
-    def inspect_artifacts(self):
+    def write_artifacts(self, artifacts, kwargs) -> None:
+        pass
+
+    def process_artifacts(self, query, kwargs):
+        pass
+
+    def inspect_artifacts(self, kwargs):
+        pass
+    
+    def read_to_artifacts(self, kwargs):
+        pass
+
+    def find(self, query_object, kwargs):
         pass
