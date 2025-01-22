c0b9c6d7977fc6baa6016da8477dd5155e25e8c5
Vedant1 <vedantiyer@hotmail.com>
2025-01-21 18:30:44 -0700

    deleted pass through code, new find function, write/process instead of put/get artifacts

--- dsi/plugins/file_writer.py
+++ dsi/plugins/file_writer.py
@@ -16,36 +16,15 @@ class FileWriter(StructuredMetadata):
             self.filenames = filenames
         else:
             raise TypeError
-        
-        '''self.file_info = {}
-        for filename in self.filenames:
-            sha = sha1(open(filename, 'rb').read())
-            self.file_info[abspath(filename)] = sha.hexdigest()'''
 
 class ER_Diagram(FileWriter):
 
-    def __init__(self, filename, dbname = None, target_table_prefix = None, **kwargs):
+    def __init__(self, filename, target_table_prefix = None, **kwargs):
         super().__init__(filename, **kwargs)
         self.output_filename = filename
         self.target_table_prefix = target_table_prefix
-        #COMMENT OUT DBNAME VARIABLE ONCE DELETING export_to_erd
-        self.dbname = dbname
 
     def get_rows(self, collection) -> None:
-        """
-        Function that outputs a ER diagram for the given database.
-
-        `dbname`: database to create an ER diagram for
-
-        `fname`: name (including path) of the image file that contains the generated ER diagram - default png if not specified
-
-        `return`: none
-        """
-        # if self.dbname is not None:
-        #     self.export_erd(self.dbname, self.output_filename)
-        #     return
-
-        # else:
         file_type = ".png"
         if self.output_filename[-4:] == ".png" or self.output_filename[-4:] == ".pdf" or self.output_filename[-4:] == ".jpg":
             file_type = self.output_filename[-4:]
@@ -130,126 +109,19 @@ class ER_Diagram(FileWriter):
         else:
             dot.render(self.output_filename, cleanup=True)
 
-    #REALLLLLY OLD CODE
-    # def export_erd(self, dbname, fname):
-    #     """
-    #     Function that outputs a ER diagram for the given database.
-
-    #     `dbname`: database to create an ER diagram for
-
-    #     `fname`: name (including path) of the image file that contains the generated ER diagram - default png if not specified
-
-    #     `return`: none
-    #     """
-        
-    #     db = sqlite3.connect(dbname)
-
-    #     file_type = ".png"
-    #     if fname[-4:] == ".png" or fname[-4:] == ".pdf" or fname[-4:] == ".jpg":
-    #         file_type = fname[-4:]
-    #         fname = fname[:-4]
-    #     elif fname[-5:] == ".jpeg":
-    #         file_type = fname[-5:]
-    #         fname = fname[:-5]
-
-    #     dot_file = open(fname + ".dot", "w")
-
-    #     numColsERD = 1
-
-    #     dot_file.write("digraph sqliteschema { ")
-    #     dot_file.write("node [shape=plaintext]; ")
-    #     dot_file.write("rankdir=LR ")
-    #     dot_file.write("splines=true ")
-    #     dot_file.write("overlap=false ")
-
-    #     list_db_tbls = "SELECT tbl_name, NULL AS label, NULL AS color, NULL AS clusterid FROM sqlite_master WHERE type='table'"
-    #     try:    
-    #         tbl_list_stmt = db.execute(list_db_tbls)
-    #     except sqlite3.Error as er:
-    #         dot_file.write(er.sqlite_errorname)
-    #         dot_file.write("Can't prepare table list statement")
-    #         db.close()
-    #         dot_file.close()
-
-    #     for row in tbl_list_stmt:
-    #         tbl_name = row[0]
-
-    #         tbl_info_sql = f"PRAGMA table_info({tbl_name})"
-    #         try:
-    #             tbl_info_stmt = db.execute(tbl_info_sql)
-    #         except sqlite3.Error as er:
-    #             dot_file.write(er.sqlite_errorname)
-    #             dot_file.write(f"Can't prepare table info statement on table {tbl_name}") 
-    #             db.close()
-    #             dot_file.close()
-
-    #         dot_file.write(f"{tbl_name} [label=<<TABLE CELLSPACING=\"0\"><TR><TD COLSPAN=\"{numColsERD}\"><B>{tbl_name}</B></TD></TR>")
-
-    #         curr_row = 0
-    #         inner_brace = 0
-    #         for info_row in tbl_info_stmt:
-    #             if curr_row % numColsERD == 0:
-    #                 inner_brace = 1
-    #                 dot_file.write("<TR>")
-
-    #             dot_file.write(f"<TD PORT=\"{info_row[1]}\">{info_row[1]}</TD>")
-    #             curr_row += 1
-    #             if curr_row % numColsERD == 0:
-    #                 inner_brace = 0
-    #                 dot_file.write("</TR>")
-
-    #         if inner_brace:
-    #             dot_file.write("</TR>")
-    #         dot_file.write("</TABLE>>]; ")
-
-    #     tbl_list_stmt = db.execute(list_db_tbls)
-    #     for row in tbl_list_stmt:
-    #         tbl_name = row[0]
-
-    #         fkey_info_sql = f"PRAGMA foreign_key_list({tbl_name})"
-    #         try:
-    #             fkey_info_stmt = db.execute(fkey_info_sql)
-    #         except sqlite3.Error as er:
-    #             dot_file.write(er.sqlite_errorname)
-    #             dot_file.write(f"Can't prepare foreign key statement on table {tbl_name}")
-    #             db.close()
-    #             dot_file.close()
-
-    #         for fkey_row in fkey_info_stmt:
-    #             dot_file.write(f"{tbl_name}:{fkey_row[3]} -> {fkey_row[2]}:{fkey_row[4]}; ")
-
-    #     dot_file.write("}")
-    #     db.close()
-    #     dot_file.close()
-
-    #     subprocess.run(["dot", "-T", file_type[1:], "-o", fname + file_type, fname + ".dot"])
-    #     os.remove(fname + ".dot")
-
 class Csv_Writer(FileWriter):
     """
     A Plugin to output queries as CSV data
     """
-
-    # This turns on strict_mode when reading in multiple csv files that need matching schemas.
-    # Default value is False.
-    strict_mode = False
-
     def __init__(self, table_name, filename, cols_to_export = None, **kwargs):
         '''
         `table_name`: name of table to be exported to a csv
-        `filename`: name of output file that the table will be stored in
+        `filename`: name of the output file where the table will be stored
         '''
         super().__init__(filename, **kwargs)
         self.csv_file_name = filename
         self.table_name = table_name
         self.export_cols = cols_to_export
-        
-
-    # def pack_header(self) -> None:
-    #     """ Set schema based on the CSV columns """
-
-    #     column_names = list(self.file_info.keys()) + list(self.csv_data.keys())
-    #     self.set_schema(column_names)
 
     def get_rows(self, collection) -> None:
         if self.table_name not in collection.keys():
@@ -266,69 +138,6 @@ class Csv_Writer(FileWriter):
                 except:
                     raise ValueError(f"Could not export to csv as specified column input {self.export_cols} is incorrect")
         df.to_csv(self.csv_file_name, index=False)
-
-        
-    # # Given an output of a sql query, reformat and write a csv of the subset data
-    # def export_csv_query(self,query,fname):
-    #     """
-    #     Function that outputs a csv file of a return query, given an initial query.
-
-    #     `query`: raw SQL query to be executed on current table
-
-    #     `fname`: target filename (including path) that will output the return query as a csv file
-
-    #     `return`: none
-    #     """
-    #     #if isVerbose:
-    #     #   print(query)
-
-    #     # Parse the table out of the query
-    #     tname = query.split("FROM ",1)[1]
-    #     tname = tname[:-1]
-
-    #     self.cur = self.con.cursor()
-    #     cdata = self.con.execute(f'PRAGMA table_info({tname});').fetchall()
-    #     cnames = [entry[1] for entry in cdata]
-    #     #if isVerbose:
-    #     #    print(cnames)
-
-    #     with open(fname,"w+") as ocsv:
-    #         csvWriter = csv.writer(ocsv,delimiter=',')
-    #         csvWriter.writerow(cnames)
-
-    #         for row in query:
-    #             print(row)
-    #             csvWriter.writerow(row)
-        
-    #     return 1
-    
-    # # Given an output of a list, reformat and write a csv of the subset data
-    # def export_csv(self,qlist,tname,fname):
-    #     """
-    #     Function that outputs a csv file of a return query, given an initial query.
-
-    #     `qlist`: a python list to be executed on current table
-
-    #     `tname`: a sql table name that originated qlist
-
-    #     `fname`: target filename (including path) that will output the return query as a csv file
-
-    #     `return`: none
-    #     """
-
-    #     self.cur = self.con.cursor()
-    #     cdata = self.con.execute(f'PRAGMA table_info({tname});').fetchall()
-    #     cnames = [entry[1] for entry in cdata]
-
-    #     with open(fname,"w+") as ocsv:
-    #         csvWriter = csv.writer(ocsv,delimiter=',')
-    #         csvWriter.writerow(cnames)
-
-    #         for row in qlist:
-    #             print(row)
-    #             csvWriter.writerow(row)
-        
-    #     return 1
 
 class Table_Plot(FileWriter):
     '''
--- examples/coreterminal.py
+++ examples/coreterminal.py
@@ -3,15 +3,15 @@ from dsi.core import Terminal
 
 '''This is an example workflow using core.py'''
 
-a=Terminal(debug_flag=False)
+a=Terminal(debug_flag=False, run_table_flag=True)
 
-# a.load_module('plugin','Bueno','reader', filenames=['data/bueno1.data', 'data/bueno2.data'])
+a.load_module('plugin','Bueno','reader', filenames=['data/bueno1.data', 'data/bueno2.data'])
 # a.load_module('plugin','Hostname','reader')
 
 # a.load_module('plugin', 'Schema', 'reader', filename="data/example_schema.json", target_table_prefix = "student")
-# a.load_module('plugin', 'YAML1', 'reader', filenames=["data/student_test1.yml", "data/student_test2.yml"], target_table_prefix = "student")
-# a.load_module('plugin', 'TOML1', 'reader', filenames=["data/results.toml", "data/results1.toml"], target_table_prefix = "results")
-a.load_module('plugin', 'MetadataReader1', 'reader', filenames=["data/metadata.json"])
+a.load_module('plugin', 'YAML1', 'reader', filenames=["data/student_test1.yml", "data/student_test2.yml"], target_table_prefix = "student")
+a.load_module('plugin', 'TOML1', 'reader', filenames=["data/results.toml", "data/results1.toml"], target_table_prefix = "results")
+# a.load_module('plugin', 'MetadataReader1', 'reader', filenames=["data/metadata.json"])
 
 # a.load_module('plugin', "Table_Plot", "writer", table_name = "student__physics", filename = "student__physics")
 # a.load_module('plugin', 'ER_Diagram', 'writer', filename = 'er_diagram.pdf')#, target_table_prefix = "physics")
@@ -20,10 +20,14 @@ a.load_module('plugin', 'MetadataReader1', 'reader', filenames=["data/metadata.j
 a.load_module('backend','Sqlite','back-write', filename='data/data.db')
 # a.load_module('backend','Parquet','back-write',filename='data/bueno.pq')
 
-a.artifact_handler(interaction_type='put')
-# data = a.artifact_handler(interaction_type='get', query = "SELECT * FROM runTable;")#, isVerbose = True)
+a.artifact_handler(interaction_type='write')
+# data = a.artifact_handler(interaction_type='process', query = "SELECT * FROM runTable;")#, isVerbose = True)
 # print(data)
 # a.artifact_handler(interaction_type="inspect")
+
+# data = a.find("data.db", "student")
+# data = a.find("data.db", "student", colFlag = True) #return list of col names from the table instead of the whole table
+# print(data)
 
 # a.unload_module('backend', 'Sqlite', 'back-write')
 
