29f8f06c5c453bc6b102475a1cc9f7a0370d16f6
Vedant1 <vedantiyer@hotmail.com>
2025-03-05 23:52:09 -0700

    updated dsi docs version and sections

--- docs/conf.py
+++ docs/conf.py
@@ -5,11 +5,12 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+exec(open("../dsi/_version.py").read())
 
 project = 'DSI'
-copyright = '2023, Triad National Security, LLC. All rights reserved.'
+copyright = '2025, Triad National Security, LLC. All rights reserved.'
 author = 'The DSI Project team'
-release = '0.0.0'
+release = __version__
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
