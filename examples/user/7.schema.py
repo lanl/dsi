31ef3a43a0d43b362490d443f202b5d773701b3b
Vedant1 <vedantiyer@hotmail.com>
2025-05-20 15:10:14 -0600

    updated user, wildfire, pennant examples with correct python api

--- examples/user/7.schema.py
+++ examples/user/7.schema.py
@@ -3,23 +3,13 @@ from dsi.dsi import DSI
 
 schema_dsi = DSI()
 
-# loads a complex schema into DSI to apply to a database
+# dsi.schema(filename)
+schema_dsi.schema("../data/example_schema.json")
+
 #dsi.read(filename, reader)
-schema_dsi.schema("../data/example_schema.json") # view comments in dsi/data/example_schema.json on how to structure it
 schema_dsi.read("../data/student_test1.yml", 'YAML1')
 
 #dsi.write(filename, writer)
 schema_dsi.write("schema_er_diagram.png", "ER_Diagram")
 
 schema_dsi.close()
-
-# DSI without a complex Schema
-basic_dsi = DSI()
-
-#dsi.read(filename, reader)
-basic_dsi.read("../data/student_test1.yml", 'YAML1')
-
-#dsi.write(filename, writer)
-basic_dsi.write("normal_er_diagram.png", "ER_Diagram") # schema_er_diagram.png will be different due to complex schema
-
-basic_dsi.close()
--- examples/wildfire/wildfire.py
+++ examples/wildfire/wildfire.py
@@ -13,14 +13,14 @@ if __name__ == "__main__":
 
     dsi = DSI()
 
-    dsi.read(input_csv, "Ensemble", table_name=table_name)
-    dsi.read(datacard, "Oceans11Datacard")
-    dsi.write(output_csv, "Csv_Writer", table_name=table_name)
-
     if os.path.exists(db_name):
         os.remove(db_name)
-    
     dsi.backend(db_name, backend_name="Sqlite")
-    dsi.ingest()
+
+    dsi.read(input_csv, "Ensemble", table_name=table_name)
+    dsi.read(datacard, "Oceans11Datacard")
+    
+    dsi.write(output_csv, "Csv_Writer", table_name=table_name)
+
     dsi.display(table_name, display_cols=columns_to_keep)
 
