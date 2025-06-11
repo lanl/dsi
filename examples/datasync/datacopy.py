3e78a146e31ce473bd16a1676f0744131091103a
Jesus Pulido <jpulidojr@gmail.com>
2025-06-11 12:05:04 -0600

    updated Core Sync class for data movement

--- examples/datasync/datacopy.py
+++ examples/datasync/datacopy.py
@@ -1,10 +1,11 @@
 from dsi.core import Sync
 
 #Origin
-local_files = "H:/Data/Scratch/shaped2d/"
+local_files = "/Users/Shared/dev/dsi/examples/clover3d/"
 #Remote
-remote_path = "H:/tmp/"
+remote_path = "/Users/Shared/staging/"
 
 # Create Sync type with project name
-s = Sync("shape2d")
-s.copy(local_files,remote_path, True)
+s = Sync("clover3d")
+s.populate(local_files,remote_path,True)
+#s.copy("copy",True)
