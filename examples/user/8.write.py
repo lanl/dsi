c5018b80ddbaaa8436f225f35a5dd07a3c684484
Vedant1 <vedantiyer@hotmail.com>
2025-07-23 11:14:16 -0600

    updated user/dev examples with paths and method changes

--- examples/user/8.write.py
+++ examples/user/8.write.py
@@ -4,6 +4,7 @@ from dsi.dsi import DSI
 write_dsi = DSI("schema_data.db") # Assuming schema_data.db has data from 7.schema.py:
 
 #dsi.write(filename, writer, table)
+# EXECUTE requirements.extras.txt  to be able to run this
 write_dsi.write("er_diagram.png", "ER_Diagram")
 
 write_dsi.write("input_table_plot.png", "Table_Plot", "input")
