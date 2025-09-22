a5dde18c2cccf3b8128c4836486aed477c73dafa
Vedant1 <vedantiyer@hotmail.com>
2025-09-21 18:38:57 -0600

    updated oceans11 datacard reader and examples with current structure

--- dsi/plugins/file_reader.py
+++ dsi/plugins/file_reader.py
@@ -747,7 +747,7 @@ class Oceans11Datacard(FileReader):
             for element, val in data.items():
                 if element not in ['authorship', 'data']:
                     if isinstance(val, list):
-                        val = ", ".join(val)
+                        val = ",, ".join(val)
                     if element not in temp_data.keys():
                         temp_data[element] = [val]
                     else:
@@ -755,15 +755,17 @@ class Oceans11Datacard(FileReader):
                     field_names.append(element)
                 else:
                     for field, val2 in val.items():
+                        if isinstance(val2, list):
+                            val2 = ",, ".join(val2)
                         if field not in temp_data.keys():
                             temp_data[field] = [val2]
                         else:
                             temp_data[field].append(val2)
                         field_names.append(field)
 
-            if sorted(field_names) != sorted(["name", "description", "keywords", "data_uses", "creators", "creation_date", 
-                                              "la_ur", "owner", "funding", "publisher", "published_date", "origin_location", 
-                                              "num_simulations", "version", "license", "live_dataset"]):
+            if sorted(field_names) != sorted(["title", "description", "keywords", "instructions_of_use", "authors", 
+                                              "release_date", "la_ur", "funding", "rights", "file_types", "num_simulations", 
+                                              "file_size", "num_files", "dataset_size", "version", "doi"]):
                 return (ValueError, f"Error in reading {filename} data card. Please ensure all fields included match the template")
 
         self.datacard_data["oceans11_datacard"] = temp_data
