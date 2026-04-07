09ea37e21db4063e35fe43b83217ee7a983c3a66
Jesus Pulido <jpulidojr@gmail.com>
2026-04-06 18:39:08 -0600

    updated old, new example with get and repo sources

---
+++ examples/user/11.get.py
@@ -0,0 +1,10 @@
+from dsi.sync import Sync
+
+# Origin location of data
+remote_sources = "/Users/Shared/dev/dsi/examples/federated/input.yaml"
+# Remote Location where database and data will be moved
+workspace = "dsi_data"
+
+# Create Sync type with project database name
+s = Sync()
+s.get(remote_sources,workspace)
