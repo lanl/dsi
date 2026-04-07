58edbfdfd2ce3e139fd5c4eb9761be4499ee73db
Jesus Pulido <jpulidojr@gmail.com>
2026-04-07 12:25:01 -0600

    added f_dsi to the example

--- examples/user/11.get.py
+++ examples/user/11.get.py
@@ -1,4 +1,5 @@
 from dsi.sync import Sync
+from dsi.utils.dsi_utils import f_dsi
 
 # Origin location of data
 remote_sources = "../federated/input.yaml"
@@ -8,3 +9,6 @@ workspace = "dsi_data"
 # Create Sync type with project database name
 s = Sync()
 s.get(remote_sources,workspace)
+
+federated_databases = f_dsi(workspace, operating_mode="notebook")
+federated_databases.f_display_databases()
