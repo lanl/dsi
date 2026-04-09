23cf362ed6a1c6479253b51075336866b7d4d0cf
Hugh Greenberg <hng@lanl.gov>
2026-04-09 15:26:19 -0600

    Removed conflict lines

--- dsi/backends/tests/test_gufi.py
+++ dsi/backends/tests/test_gufi.py
@@ -1,22 +1,14 @@
 from dsi.backends.gufi import Gufi
 
 def test_artifact_query():
-<<<<<<< Updated upstream
-    gufi_index_path = "/opt/example-collection"
-=======
-#    gufi_index_path = "/opt/example-collection"
     gufi_index_path = "/opt/example-collection/tests/images"
->>>>>>> Stashed changes
     gufi_prefix = "/home/hgreenburg/GUFI/build/"
     dsi_table_name = "wfdata"
     dsi_file_path_column = "LOCAL_PATH"
     dsi_columns = ["sim_id", "wind_speed"]
     gufi_columns = ["fullpath", "size", "mtime"]
-<<<<<<< Updated upstream
     collection_name = "collection1234"
-=======
     collection_name = "collection"
->>>>>>> Stashed changes
     gufi_tag_tool_path = "/home/hgreenburg/gufi-dsi-tag-user-namespace/target/release/gufi-dsi-tag"
     from dsi.core import Sync
     s = Sync("collection")
