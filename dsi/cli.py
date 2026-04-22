cb9df1e689ac20c15c56284fc5b95fc3db3e7dcc
Vedant Iyer <vedantiyer@hotmail.com>
2026-04-22 12:31:18 -0600

    added ndp to cli -b flag help

--- dsi/cli.py
+++ dsi/cli.py
@@ -898,7 +898,7 @@ def main():
 
     args = parser.parse_args()
     if args.backend.lower() not in ["sqlite", "duckdb", "ndp"]:
-        print("ERROR: Invalid backend input. Valid backends are: sqlite, duckdb, ndp")
+        print("ERROR: Invalid backend input. Valid backends are: sqlite, duckdb, and ndp")
         exit(1)
     print("   ", textwrap.dedent(fr"""
          _____           ___
