ee7a1c2b861e9c12243d5fee563f78616c9ae697
Vedant1 <vedantiyer@hotmail.com>
2026-05-22 13:24:34 -0600

    updated error handling error message for conduit

--- dsi/sync.py
+++ dsi/sync.py
@@ -254,9 +254,9 @@ class Sync():
         if process.returncode != 0:
             if "too many authentication failures" in str(stderr).lower():
                 raise RuntimeError(f"{cmd_name} failed due to multiple incorrect password attempts. Check the password and remote path.")
-            elif "No credentials" in stdout:
+            elif "No credentials" in stderr:
                 print("Kerberos authentication error: No credentials found. Please type 'conduit get' to issue a ticket.")
-                raise RuntimeError("Kerberos message: " + str(stdout))
+                raise RuntimeError("Kerberos message: " + str(stderr))
             raise RuntimeError(f"{cmd_name} failed: \n{stderr}")
         return stdout
 
