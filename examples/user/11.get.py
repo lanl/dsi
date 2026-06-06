6e29650a892b8ce712254b58e5d46838467cb47e
Vedant1 <vedantiyer@hotmail.com>
2026-06-05 23:55:22 -0600

    simplified sync.get and cli.pull_data inputs

--- examples/user/11.get.py
+++ examples/user/11.get.py
@@ -7,12 +7,12 @@ workspace = "dsi_data"
 
 # Create Sync type with project database name
 s = Sync("federated.db")
-s.get(input_yaml=remote_sources,workspace_folder=workspace)
+s.get(config_file=remote_sources, workspace_folder=workspace)
 
 # Download data referenced in this database -- db must have been previously indexed by DSI
-s.get_data("ocean_11_datasets.db", workspace)
+s.get_data(db_name="ocean_11_datasets.db", workspace_folder=workspace)
 
 
 from dsi.dsifederated import DSIFederated
 federated_databases = DSIFederated(workspace, operating_mode="notebook")
-federated_databases.f_display_databases()
+federated_databases.f_list_databases()
